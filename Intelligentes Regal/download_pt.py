374e53756cd0f2c33726f4af772d150704c425d8
LeonardIfiii <132555068+LeonardIfiii@users.noreply.github.com>
2025-07-02 14:49:16 +0200

    Delete Intelligentes Regal directory

--- Intelligentes Regal/download_pt.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# YOLOv8s Modell herunterladen
-model = YOLO("yolov8s")
-
-# Speichern des Modells als .pt Datei
-model.export(format="torchscript")  # Speichert als .pt Datei
